77b33d92a0c31a924ef0f88ca29a172caf792fa1
Diego Doná <diedona@gmail.com>
2019-10-22 18:42:47 -0300

    web api to test repository / interfaces

---
+++ DDona.RepositoryPattern.WebApi/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DDona.RepositoryPattern.Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDona.RepositoryPattern.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(_personRepository.Get());
+        }
+
+        [Route("inactive")]
+        public IActionResult GetInative()
+        {
+            return Ok(_personRepository.GetInactive());
+        }
+    }
+}
