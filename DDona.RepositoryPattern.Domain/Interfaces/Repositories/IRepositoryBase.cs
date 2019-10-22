b72296adea3032fe0bbafd0ba8cadeaf865b6906
Diego Doná <diedona@gmail.com>
2019-10-22 16:46:17 -0300

    interface for repository base

---
+++ DDona.RepositoryPattern.Domain/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.RepositoryPattern.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryBase<T> where T: IEntity
+    {
+        IList<T> Get();
+        T GetById(int id);
+        void Save(T entity);
+        void Delete(T entity);
+        void Delete(int id);
+        void Update(T entity);
+    }
+}
